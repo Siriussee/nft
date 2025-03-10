60f8a665c26bf4f30fefe99d4b5b7e901fd3b81a
siriussee <sirius_see@outlook.com>
2025-03-10 01:42:38 -0700

    save progress

--- README.md
+++ README.md
@@ -11,3 +11,113 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/filename.js
 ```
+
+## Understand NFT
+
+Please read: [ERC-721: Non-Fungible Token Standard](https://eips.ethereum.org/EIPS/eip-721)
+
+## Store NFT Image at ...
+
+### ...  a server, just like what we always do
+
+Go to [OpenSea](https://opensea.io/), an NFT marketplace, pick a random NFT collection, pick a random NFT.
+
+Let's say I pick [Fidenza #956](https://opensea.io/assets/ethereum/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270/78000956).
+
+From its details, I found its contract address and token id.
+
+```
+Contract Address 0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270
+Token ID  78000956
+```
+
+As they contains URLs, we can just go to the contract and token URI
+- [contract address](https://etherscan.io/address/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270#readContract)
+- [NFT URI](https://token.artblocks.io/78000956)
+
+We found that the `primary_asset_url`
+```
+{
+    "primary_asset_url": "https://media-proxy.artblocks.io/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270/78000956.png"
+}
+```
+
+And it points us to an [Amazon S3 bucket](https://artblocks-mainnet.s3.amazonaws.com/78000956.png).
+
+### ... a decentralized server, so it won't get down easily
+
+For example, I pick [Pudgy Penguin #7183](https://opensea.io/assets/ethereum/0xbd3531da5cf5857e7cfaa92426877b022e612cf8/7183) and go to tis token URI. I get a link starting with `ipfs.io/ipfs/`, which following contents
+```
+{
+  "attributes": [
+    {
+      "trait_type": "Background",
+      "value": "Blue"
+    },
+    {
+      "trait_type": "Skin",
+      "value": "Light Gray"
+    },
+    {
+      "trait_type": "Body",
+      "value": "Apron"
+    },
+    {
+      "trait_type": "Face",
+      "value": "Cross Eyed"
+    },
+    {
+      "trait_type": "Head",
+      "value": "Party Hat"
+    }
+  ],
+  "description": "A collection 8888 Cute Chubby Pudgy Penquins sliding around on the freezing ETH blockchain.",
+  "image": "ipfs://QmNf1UsmdGaMbpatQ6toXSkzDpizaGmC9zfunCyoz1enD5/penguin/7183.png",
+  "name": "Pudgy Penguin #7183"
+}
+```
+
+It contains all attributes to the Penguin, but I want to access its image. Simply copy-pasting the IPFS to web browser won't work. Unlike we we experienced with AWS S3.
+```
+{
+    "image": "ipfs://QmNf1UsmdGaMbpatQ6toXSkzDpizaGmC9zfunCyoz1enD5/penguin/7183.png"
+}
+```
+
+It is because the image is stored at IPFS's server(s), and our web browser won't process the `ipfs://` protocol as it does to `https://`. We need an "gateway" to access it. [ipfs.io](https://ipfs.io/) is usually a good choice.
+
+> For other choices, see [Public Gateways Checker](https://ipfs.github.io/public-gateway-checker/)
+
+You need to append the content after `ipfs://` to the end of `https://ipfs.io/ipfs/` to get it:
+```
+https://ipfs.io/ipfs/QmNf1UsmdGaMbpatQ6toXSkzDpizaGmC9zfunCyoz1enD5/penguin/7183.png
+```
+
+[ipfs.io](https://ipfs.io/) may go down. But IPFS itself is unlikely to. For the reason, please refer to [IPFS Docs](https://docs.ipfs.tech/concepts/lifecycle/#_1-content-addressing-merkleizing). Simply put, it is like bittorrent: as long as there is a copy in the IPFS network, you can get it.
+
+## Deploy my own NFT
+
+### Preparation
+
+node 18 and yarn are required for this demo
+```
+nvm install 18
+nvm use 18
+npm install -g yarn
+```
+
+Install dependencies
+```
+yarn install 
+```
+
+### Project structure
+
+- `contracts`
+    - `RandomIpfsNft.sol`
+- `scripts`
+    - `01-deploy-random-ipfs-nft.js`
+    - `02-mint.js`
+
+### Deploy contracts 
+
--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -1,46 +1,78 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
-pragma solidity ^0.8.19;
-contract RandomIpfsNft is ERC721URIStorage, VRFConsumerBaseV2 {
 
+/**
+ * @title RandomIpfsNft
+ * @dev This contract allows users to mint random NFTs based on a Chainlink VRF-derived random number.
+ * Each NFT represents a different breed of dog, and the breed is determined by the randomness provided.
+ */
+contract RandomIpfsNft is ERC721URIStorage, VRFConsumerBaseV2 {
+    
+    // Chainlink VRF Coordinator for getting random values
     VRFCoordinatorV2Interface immutable i_vrfCoordinator;
+
+    // Key hash for gas lane
     bytes32 immutable i_gasLane;
+
+    // Chainlink subscription ID
     uint64 immutable i_subscriptionId;
+
+    // Gas limit for the callback function
     uint32 immutable i_callBackGasLimit;
 
+    // Constants for random number generation
     uint16 constant REQUEST_CONFIRMATIONS = 3;
     uint32 constant NUM_WORDS = 1;
     uint256 constant MAX_CHANCE_VALUE = 100;
 
-    mapping (uint256=>address) s_requestIdToSender;
+    // Mapping from request ID to the address of the requester
+    mapping (uint256 => address) s_requestIdToSender;
+
+    // IPFS URIs for different dog breeds
     string[3] s_dogTokenUris;
 
+    // Counter for minted tokens
     uint256 s_tokenCounter;
 
+    /**
+     * @dev Constructor to initialize the contract with necessary parameters.
+     * @param vrfCoordinatorV2 Address of the VRF Coordinator contract
+     * @param gasLane Key hash for gas lane
+     * @param subscriptionId Subscription ID for Chainlink VRF
+     * @param callBackGasLimit Gas limit for the callback function
+     * @param dogTokenUris Array of URIs for different dog breeds
+     */
     constructor(
         address vrfCoordinatorV2, 
         bytes32 gasLane, 
         uint64 subscriptionId,
         uint32 callBackGasLimit,
         string[3] memory dogTokenUris
-    ) ERC721 ("Random IPFS NFT", "RIN") VRFConsumerBaseV2(vrfCoordinatorV2){
+    ) 
+        ERC721("Random IPFS NFT", "RIN") 
+        VRFConsumerBaseV2(vrfCoordinatorV2)
+    {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callBackGasLimit = callBackGasLimit;
         s_tokenCounter = 0;
         s_dogTokenUris = dogTokenUris;
-            // 0 = st.Bernard
-            // 1 = Pug
-            // 2 = Shiba inu
-
+        // 0 = St. Bernard
+        // 1 = Pug
+        // 2 = Shiba Inu
     }
 
+    /**
+     * @dev Requests a random dog minting.
+     * @return requestId The request ID for the random number generation
+     */
     function requestDoggie() public returns (uint256 requestId) {
-        requestId = i_vrfCoordinator.requestRandomWords (
+        requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
             REQUEST_CONFIRMATIONS,
@@ -50,37 +82,49 @@ contract RandomIpfsNft is ERC721URIStorage, VRFConsumerBaseV2 {
         s_requestIdToSender[requestId] = msg.sender;
     } 
 
+    /**
+     * @dev Callback function used by VRF Coordinator to return the random number.
+     * @param requestId The ID of the request
+     * @param randomWords The array of random words generated
+     */
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
-        //owner of the dog
+        // Owner of the dog NFT
         address dogOwner = s_requestIdToSender[requestId];
         uint256 newTokenId = s_tokenCounter;
-        s_tokenCounter =s_tokenCounter + 1;
-        // did we get a random dog?
-        // is the st.bernard super random?
-        // no, we need to fix it
+        s_tokenCounter = s_tokenCounter + 1;
 
-        //randomWords = 2151846168646848786468
-        //get the breed?
+        // Get the breed from the random number
         uint256 moddedRng = randomWords[0] % MAX_CHANCE_VALUE;
         uint256 breed = getBreedFromModdenRng(moddedRng);
 
+        // Mint the NFT and set its token URI
         _safeMint(dogOwner, newTokenId);
         _setTokenURI(newTokenId, s_dogTokenUris[breed]);
     }
 
+    /**
+     * @dev Generates an array representing the chances for each breed of dog.
+     * @return An array containing the cumulative chances for different breeds
+     */
     function getChanceArray() public pure returns(uint256[3] memory) {
-        // 0 - 9 = st.bernard
-        // 10 -29 = pug
-        // 30 - 99 = shiba inu
-        return [10, 30, MAX_CHANCE_VALUE];
+        // 0 - 9 = St. Bernard
+        // 10 - 29 = Pug
+        // 30 - 99 = Shiba Inu
+        return [10, 20, MAX_CHANCE_VALUE];
     }
 
-    function getBreedFromModdenRng(uint256 moddendRng) private pure returns(uint256)  {
+    /**
+     * @dev Determines the breed of the dog based on the generated random number.
+     * @param moddendRng The random number modded to the maximum chance value
+     * @return The index of the breed
+     */
+    function getBreedFromModdenRng(uint256 moddendRng) private pure returns(uint256) {
         uint256 cumulativeSum = 0;
         uint256[3] memory chanceArray = getChanceArray();
         uint256 i;
-        for(i = 0; i<chanceArray.length; i++) {
-            if(moddendRng >=cumulativeSum && moddendRng < cumulativeSum + chanceArray[i]){
+        
+        for (i = 0; i < chanceArray.length; i++) {
+            if (moddendRng >= cumulativeSum && moddendRng < cumulativeSum + chanceArray[i]) {
                 break;
             }
             cumulativeSum = cumulativeSum + chanceArray[i];
